5f2930b9d3a161611e4fb95736cc4df34e7c70d8
Willy McNamara <wmcnamara11@gmail.com>
2023-04-26 08:55:49 -0500

    Added indexing to database and switched database to the non-test title atelier_db

--- src/database/database.ts
+++ src/database/database.ts
@@ -11,7 +11,7 @@ const user = process.env.PSQL_USER as string
 const password = process.env.PSQL_PASSWORD as string
 const host = process.env.PSQL_HOST as string
 
-export const sequelize = new Sequelize('atelier_db_test', user, password, {
+export const sequelize = new Sequelize('atelier_db', user, password, {
   host: host,
   dialect: 'postgres',
   logging: false
